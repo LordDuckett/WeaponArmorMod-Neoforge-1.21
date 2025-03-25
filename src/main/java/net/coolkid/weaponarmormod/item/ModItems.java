a17e2626050edad4246dc471d164ada1f4b6f518
LordDuckett <duckett.ethan@gmail.com>
2025-03-24 19:04:25 -0700

    Warden Tendril Description

--- src/main/java/net/coolkid/weaponarmormod/item/ModItems.java
+++ src/main/java/net/coolkid/weaponarmormod/item/ModItems.java
@@ -1,10 +1,15 @@
 package net.coolkid.weaponarmormod.item;
 
 import net.coolkid.weaponarmormod.WAM;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(WAM.MOD_ID);
@@ -15,7 +20,13 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
 
     public static final DeferredItem<Item> WARDEN_TENDRIL = ITEMS.register("warden_tendril",
-            () -> new Item(new Item.Properties().food(ModFoodProperties.WARDEN_TENDRIL)));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.WARDEN_TENDRIL)) {
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+                    tooltipComponents.add(Component.translatable("tooltip.weaponarmormod.warden_tendril"));
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
 
     public static void register(IEventBus modEventBus) { ITEMS.register(modEventBus);}
 }
